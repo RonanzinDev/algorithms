3a905fcb1b2b3fc00594ce060a06bd01d1ddb19d
ronanzindev <morellianogm@gmail.com>
2024-04-20 11:51:13 -0300

    lru_cache

---
+++ lru_cache/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "container/list"
+
+type (
+	LRUCache struct {
+		capacity int
+		items    map[int]*list.Element
+		queue    *list.List
+	}
+	cacheItem struct {
+		key   int
+		value interface{}
+	}
+)
+
+func NewLRUCache(cap int) *LRUCache {
+	return &LRUCache{
+		capacity: cap,
+		items:    make(map[int]*list.Element),
+		queue:    list.New(),
+	}
+}
+
+func (c *LRUCache) Get(key int) interface{} {
+	if elem, found := c.items[key]; found {
+		c.queue.MoveToFront(elem)
+		return elem.Value.(*cacheItem).value
+	}
+	return -1
+}
+func (c *LRUCache) Set(key int, value interface{}) {
+	if elem, found := c.items[key]; found {
+		c.queue.MoveToFront(elem)
+		elem.Value.(*cacheItem).value = value
+		return
+	}
+	if c.queue.Len() == c.capacity {
+		oldest := c.queue.Back()
+		delete(c.items, oldest.Value.(*cacheItem).key)
+		c.queue.Remove(oldest)
+	}
+	elem := c.queue.PushFront(&cacheItem{key, value})
+	c.items[key] = elem
+}
